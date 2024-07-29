ca888bfb3f0c7f6659a308048d718c2bceb5e68a
DaveTheRpaGuy <dave.morris.atl@gmail.com>
2024-07-28 20:39:16 -0400

    Monitoring an API > Health Checks

--- MonitoringApi/Program.cs
+++ MonitoringApi/Program.cs
@@ -1,3 +1,7 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using MonitoringApi.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks()
+    .AddCheck<RandomHealthCheck>("Site Health Check")
+    .AddCheck<RandomHealthCheck>("Database Health Check");
+
+builder.Services.AddHealthChecksUI(opts =>
+{
+    opts.AddHealthCheckEndpoint("api", "/health"); // setting this to monitor itself basically
+    opts.SetEvaluationTimeInSeconds(10); // this is aggressive, in prod maybe 5 minutes
+}).AddInMemoryStorage(); // could be put in sql server or whatever here instead of in memory
 
 var app = builder.Build();
 
@@ -21,5 +34,10 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+app.MapHealthChecksUI();
 
 app.Run();
